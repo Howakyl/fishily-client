74feaef54fdcfe529df4e0ac287fbe2bef96fa1f
Howakyl96 <howakyl@gmail.com>
2021-01-15 18:23:57 -0800

    converted postDetail to hook

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,82 +1,80 @@
-import React from 'react'; 
+import React, { useState , useEffect } from 'react'; 
 import Spinner from '../components/Spinner';
 import PostModel from '../models/post';
 import { Redirect , Link, withRouter } from 'react-router-dom';
 import './PostDetail.css';
 
-class PostDetail extends React.Component {
-    state = {
-        post: {},
-        redirectToPosts: false,
-        loading: true
-    }
+const PostDetail = (props) => {
 
-    componentDidMount() {
-        const postId = this.props.match.params.id;
+    const [ loading , setLoading ] = useState(true);
+    const [ redirectToPosts , setRedirectToPosts ] = useState(false);
+    const [ post , setPost ] = useState({});
+
+
+    useEffect (() => {
+        const postId = props.match.params.id;
         PostModel.getOne(postId)
             .then((data) => {
-                this.setState({ 
-                    post: data.data.post,
-                    loading: false
-                })
+                setPost(data.data.post);
+                setLoading(false);
             })
-    };
+    } , [props.match.params.id]);
     
-    deletePost = (id) => {
+    const deletePost = (id) => {
         PostModel.delete(id)
             .then((res) => {
-                this.setState({redirectToPosts : true})
+                setRedirectToPosts(true);
             })
-    }
+    };
 
-    confirmPostDelete (post) {
+    function confirmPostDelete (post) {
         const confirmDelete = window.confirm('Are you sure you want to delete your post?');
-        if(confirmDelete === true) return this.deletePost(post);
+        if(confirmDelete === true) return deletePost(post);
     }
     
-    renderBtns () {
+    function renderBtns () {
         
-        if (this.props.user._id === this.state.post.user._id) {
+        if (props.user._id === post.user._id) {
             return (
                 <>
                     <span
                         className="btn btn-primary"
-                        onClick={() => this.confirmPostDelete(this.state.post._id)}
+                        onClick={() => confirmPostDelete(post._id)}
                         >Delete Post</span>
                         
-                        <Link to={`/posts/${this.state.post._id}/edit`} className="btn btn-primary post-detail-edit-btn">Edit Post</Link>
+                        <Link to={`/posts/${post._id}/edit`} className="btn btn-primary post-detail-edit-btn">Edit Post</Link>
                 </>
             )
         }
     }
 
-    render () {
-        if (this.state.redirectToPosts) {
+
+        if (redirectToPosts) {
             return <Redirect to="/posts"/>
         }
 
-        if (!this.state.loading) {
+        if (!loading) {
             return (
                 <div className="container">
                     <div className="post-detail-container">
-                    <img src={this.state.post.image} alt="fish" className="post-detail-img img-fluid"/>
+                    <img src={post.image} alt="fish" className="post-detail-img img-fluid"/>
                         <section className="post-detail-info">
 
                             <div className="user-info">
-                                <Link to={`/users/${this.state.post.user._id}`}>
-                                    <img className="post-detail-user-img img-fluid" src={this.state.post.user.picture} alt={this.props.user.username}/>
+                                <Link to={`/users/${post.user._id}`}>
+                                    <img className="post-detail-user-img img-fluid" src={post.user.picture} alt={props.user.username}/>
                                 </Link>
-                                <Link to={`/users/${this.state.post.user._id}`}>
-                                    <p className="post-detail-username">{this.state.post.user.username}</p>
+                                <Link to={`/users/${post.user._id}`}>
+                                    <p className="post-detail-username">{post.user.username}</p>
                                 </Link>
                             </div>
-                            <h2>{this.state.post.title}</h2>
-                            <h5><em>Fish Caught:</em> {this.state.post.fish}</h5>
+                            <h2>{post.title}</h2>
+                            <h5><em>Fish Caught:</em> {post.fish}</h5>
                             <hr/>
-                            <p className="post-detail-description">{this.state.post.description}</p>
-                            <small>Caught at: {this.state.post.locationName}</small>
+                            <p className="post-detail-description">{post.description}</p>
+                            <small>Caught at: {post.locationName}</small>
                             <div className="post-detail-buttons">
-                                {this.renderBtns()}
+                                {renderBtns()}
                             </div>
                         </section>
                     </div>
@@ -85,7 +83,6 @@ class PostDetail extends React.Component {
         } else {
             return <Spinner />
         }
-    }
 }
 
 export default withRouter(PostDetail);
