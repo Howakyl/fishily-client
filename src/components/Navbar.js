2f6ff5855ff15f91a28eb637878411a1a32b86af
Howakyl <howakyl@gmail.com>
2021-06-10 17:01:42 -0700

    formatting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,90 +1,110 @@
-import { Link } from 'react-router-dom';
-import UserModel from '../models/user';
+import { Link } from "react-router-dom";
+import UserModel from "../models/user";
 
 const Navbar = (props) => {
-    
-    function logOutClick() {
-        
-        UserModel.logout(props)
-        .then((res) => {
-            props.setUser({});
-            localStorage.clear();
-        })
+  function logOutClick() {
+    UserModel.logout(props).then((res) => {
+      props.setUser({});
+      localStorage.clear();
+    });
+  }
+
+  function confirmLogOut() {
+    const confirmLogOut = window.confirm("are you sure you want to log out?");
+    if (confirmLogOut === true) return logOutClick();
+  }
+
+  function loginNav() {
+    if (!props.user.username) {
+      return (
+        <>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/login">
+              Log In
+            </Link>
+          </li>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/signup">
+              Sign Up
+            </Link>
+          </li>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <li className="nav-item ">
+            <Link className="nav-link" to="/" onClick={confirmLogOut}>
+              Log Out
+            </Link>
+          </li>
+        </>
+      );
     }
+  }
 
-    function confirmLogOut () {
-        const confirmLogOut = window.confirm('are you sure you want to log out?');
-        if (confirmLogOut === true) return logOutClick();
+  function renderCreatePost() {
+    if (props.user.username) {
+      return (
+        <>
+          <li className="nav-item active">
+            <Link className="nav-link" to="/posts/new">
+              New Catch
+            </Link>
+          </li>
+        </>
+      );
     }
-    
+  }
 
-    function loginNav() {
-        if(!props.user.username) {
-            return (
-                <>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/login">Log In</Link>
-                    </li>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/signup">Sign Up</Link>
-                    </li>
-                </>
-            )
-        } else {
-            return (
-                <>
-                <li className="nav-item ">
-                    <Link className="nav-link" to="/" onClick={confirmLogOut}>Log Out</Link>
-                </li>
-                </>
-            )
-        }
+  function renderProfileImg() {
+    if (props.user.username) {
+      return (
+        <Link to={`/users/${props.user._id}`}>
+          <img
+            src={props.user.picture}
+            alt="profile"
+            className="navbar-user-img"
+          />
+        </Link>
+      );
     }
+  }
 
-    function renderCreatePost() {
-        if(props.user.username) {
-            return (
-                <>
-                    <li className="nav-item active">
-                        <Link className="nav-link" to='/posts/new'>New Catch</Link>
-                    </li>
-                </>
-            )
-        }
-    }
+  return (
+    <nav className="navbar navbar-expand-md navbar-light">
+      <div className="nav-title">
+        <Link className="navbar-brand navbar-title" to="/">
+          Fishily
+        </Link>
+      </div>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#fishilyNav"
+        aria-controls="fishilyNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-    function renderProfileImg () {
-        if (props.user.username) {
-            return (
-                <Link to={`/users/${props.user._id}`}>
-                    <img src={props.user.picture} alt="profile" className="navbar-user-img"/>
-                </Link>
-            )
-        }
-    }
+      <div className="collapse navbar-collapse" id="fishilyNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/posts">
+              All Catches
+            </Link>
+          </li>
 
-    return (
-        <nav className="navbar navbar-expand-md navbar-light">
-            <div className="nav-title">
-                <Link className="navbar-brand navbar-title" to="/">Fishily</Link>
-            </div>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#fishilyNav" aria-controls="fishilyNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="fishilyNav">
-                <ul className="navbar-nav ml-auto">
-                <li className="nav-item active">
-                    <Link className="nav-link" to="/posts">All Catches</Link>
-                </li>
-
-                {renderCreatePost()}
-                {loginNav()}
-                </ul>
-                {renderProfileImg()}
-            </div>
-        </nav>
-    )
-}
+          {renderCreatePost()}
+          {loginNav()}
+        </ul>
+        {renderProfileImg()}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
--- src/models/post.js
+++ src/models/post.js
@@ -1,45 +1,43 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 let endPoint;
 
-if (process.env.NODE_ENV === 'production') {
-    endPoint = `https://fishily-api.herokuapp.com/api/fishily/posts`
+if (process.env.NODE_ENV === "production") {
+  endPoint = `https://fishily-api.herokuapp.com/api/fishily/posts`;
 } else {
-    endPoint = `http://localhost:4000/api/fishily/posts`
-};
+  endPoint = `http://localhost:4000/api/fishily/posts`;
+}
 
 class PostModel {
+  //GET all posts
+  static all = () => {
+    let request = axios.get(endPoint);
+    return request;
+  };
 
-    //GET all posts
-    static all = () => {
-        let request = axios.get(endPoint);
-        return request;
-    };
+  //GET one post
+  static getOne = (id) => {
+    let request = axios.get(`${endPoint}/${id}`);
+    return request;
+  };
 
-    //GET one post
-    static getOne = (id) => {
-        let request = axios.get(`${endPoint}/${id}`);
-        return request;
-    }
+  //CREATE post
+  static create = (post, userId) => {
+    let request = axios.post(`${endPoint}/${userId}`, post);
+    return request;
+  };
 
-    //CREATE post
-    static create = (post, userId) => {
-        let request = axios.post(`${endPoint}/${userId}` , post);
-        return request;
-    }
+  //DELETE post
+  static delete(postId) {
+    let request = axios.delete(`${endPoint}/${postId}`);
+    return request;
+  }
 
-    //DELETE post
-    static delete(postId) {
-        let request = axios.delete(`${endPoint}/${postId}`);
-        return request;
-    }
-
-    //EDIT post
-    static update(postId, updatedPost) {
-        let request = axios.put(`${endPoint}/${postId}` , updatedPost);
-        return request;
-    }
-};
+  //EDIT post
+  static update(postId, updatedPost) {
+    let request = axios.put(`${endPoint}/${postId}`, updatedPost);
+    return request;
+  }
+}
 
 export default PostModel;
