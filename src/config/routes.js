3f2897ced539d69aa452dd52b43ffef2af5f20b7
Howakyl <howakyl@gmail.com>
2021-06-10 17:03:39 -0700

    format routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -12,23 +12,22 @@ import EditPost from '../pages/EditPost';
 import EditUser from '../pages/EditUser';
 
 function Routes (props) {
-
-    return (
-        <Switch >
-            <Route exact path='/' render={() => <Home user={props.user} />}  />
-            {/* USER ROUTES */}
-            <Route exact path='/users/:id' render={() => <UserShow user={props.user} setUser={props.setUser}/>}/>
-            <Route exact path='/users' component={ UserList }/>
-            <Route exact path='/users/:id/edit' component={ EditUser }/>
-            <Route path='/login' render={() => <LogIn user={props.user} setUser={props.setUser}/>}/>
-            <Route path='/signup' render={() => <SignUp user={props.user} setUser={props.setUser}/>}/>
-            {/* POST ROUTES */}
-            <Route exact path='/posts' component={ PostList }/>
-            <Route exact path='/posts/new' render={() => <NewPost user={props.user} setUser={props.setUser}/>}/>
-            <Route exact path='/posts/:id/edit' component={ EditPost } />
-            <Route exact path='/posts/:id' render={() => <PostDetail user={props.user} setUser={props.setUser}/>} />
-        </Switch>
-    )
+  return (
+    <Switch >
+      <Route exact path='/' render={() => <Home user={props.user} />}  />
+      {/* USER ROUTES */}
+      <Route exact path='/users/:id' render={() => <UserShow user={props.user} setUser={props.setUser}/>}/>
+      <Route exact path='/users' component={ UserList }/>
+      <Route exact path='/users/:id/edit' component={ EditUser }/>
+      <Route path='/login' render={() => <LogIn user={props.user} setUser={props.setUser}/>}/>
+      <Route path='/signup' render={() => <SignUp user={props.user} setUser={props.setUser}/>}/>
+      {/* POST ROUTES */}
+      <Route exact path='/posts' component={ PostList }/>
+      <Route exact path='/posts/new' render={() => <NewPost user={props.user} setUser={props.setUser}/>}/>
+      <Route exact path='/posts/:id/edit' component={ EditPost } />
+      <Route exact path='/posts/:id' render={() => <PostDetail user={props.user} setUser={props.setUser}/>} />
+    </Switch>
+  )
 }
 
 
