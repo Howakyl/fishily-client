36d3e0040d72b326d906cbc4765d861bcf527491
Howakyl <howakyl@gmail.com>
2021-06-27 18:36:53 -0700

    render more user data on comments, style user img

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import classes from './Comment.module.css';
-
+import React from "react";
+import classes from "./Comment.module.css";
 
 const Comment = (props) => {
   return (
     <div>
+      <img src={props.comment.user.picture} alt={props.comment.user.username}  className={classes.userImg}/>
+      <p>{props.comment.user.username}</p>
       <p>{props.comment.description}</p>
     </div>
   );
-}
+};
 export default Comment;
--- src/pages/Posts/PostDetailComments.js
+++ src/pages/Posts/PostDetailComments.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import Comment from '../../components/Comments/Comment';
-import classes from './PostDetailComments.module.css';
-
+import React from "react";
+import Comment from "../../components/Comments/Comment";
+import classes from "./PostDetailComments.module.css";
 
 const PostDetailComments = (props) => {
-  console.log(props.comments)
+  console.log(props.comments);
   const renderComments = () => {
-    return props.comments.map(comment => (
-      <Comment comment={comment} key={comment._id}/>
-    ))
-  }
+    return props.comments.map((comment) => (
+      <Comment comment={comment} key={comment._id} />
+    ));
+  };
 
   return (
     <div className={classes.commentsContainer}>
       <h2>Comments</h2>
-      {props.comments.length > 0 ? renderComments() : <h2>No comments (yet!)</h2>}
+      {props.comments.length > 0 ? (
+        renderComments()
+      ) : (
+        <h2>No comments (yet!)</h2>
+      )}
     </div>
   );
-}
+};
 export default PostDetailComments;
