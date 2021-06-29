b64bcae06bb91fbcd3331b92a245923bf7ce221b
Howakyl <howakyl@gmail.com>
2021-06-28 19:10:09 -0700

    styling for comment header

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -6,10 +6,6 @@ const Comment = (props) => {
   const date = new Date(props.comment.createdAt);
   const today = new Date();
   let diff = Math.abs(today - date);
-  // console.log('seconds: ', (diff / 1000))
-  // console.log('minutes: ', (diff / 1000 / 60))
-  // console.log('hours: ', (diff / 1000 / 60 / 60)) OR 36e5
-  // console.log('DAY: ', Math.abs(today - date) / 36e5 / 24)
 
   function getDiff(diff) {
     if (diff / 36e5 < 1) {
@@ -37,13 +33,13 @@ const Comment = (props) => {
         />
       </Link>
       <div className={classes.commentInfo}>
-        <section>
+        <section className={classes.commentHeader}>
           <Link to={`/users/${props.comment.user._id}`}>
             <p className={classes.commentUsername}>
               {props.comment.user.username}
             </p>
           </Link>
-          <small>{getDiff(diff)}</small>
+          <small className={classes.postedOn}>{getDiff(diff)}</small>
         </section>
         <p className={classes.commentDescription}>
           {props.comment.description}
