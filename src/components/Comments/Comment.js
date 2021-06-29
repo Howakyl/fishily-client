18f1f3d098ed30d7782afa756f6fb7e8df9cce26
Howakyl <howakyl@gmail.com>
2021-06-28 17:41:15 -0700

    comments date comparison. need to implement past 24 hrs

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 import classes from "./Comment.module.css";
 
 const Comment = (props) => {
-  // const date = new Date(props.comment.createdAt)
-
-
+  const date = new Date(props.comment.createdAt)
+  const today = new Date()
+  // console.log(Math.abs(today - date) / 36e5)
+  let diff = Math.abs(today - date)
+  // console.log('seconds: ', (diff / 1000))
+  // console.log('minutes: ', (diff / 1000 / 60))
+  console.log('hours: ', (diff / 1000 / 60 / 60))
+  if ((diff / 1000 / 60 / 60) < 24) {
+    diff = `${Math.floor((diff / 1000 / 60 / 60))}hr`
+  }
+  console.log(diff)
   return (
     <div className={classes.commentContainer}>
       <Link to={`/users/${props.comment.user._id}`}>
@@ -22,7 +30,7 @@ const Comment = (props) => {
               {props.comment.user.username}
             </p>
           </Link>
-          <small>posted on:</small>
+          <small>{diff}</small>
         </section>
         <p className={classes.commentDescription}>
           {props.comment.description}
