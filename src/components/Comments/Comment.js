a1bed402da793ea40b885a03f1b9185104df0578
Howakyl <howakyl@gmail.com>
2021-06-28 19:00:39 -0700

    comments now show how recently they were posted, formatted

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -3,17 +3,30 @@ import { Link } from "react-router-dom";
 import classes from "./Comment.module.css";
 
 const Comment = (props) => {
-  const date = new Date(props.comment.createdAt)
-  const today = new Date()
-  // console.log(Math.abs(today - date) / 36e5)
-  let diff = Math.abs(today - date)
+  const date = new Date(props.comment.createdAt);
+  const today = new Date();
+  let diff = Math.abs(today - date);
   // console.log('seconds: ', (diff / 1000))
   // console.log('minutes: ', (diff / 1000 / 60))
-  console.log('hours: ', (diff / 1000 / 60 / 60))
-  if ((diff / 1000 / 60 / 60) < 24) {
-    diff = `${Math.floor((diff / 1000 / 60 / 60))}hr`
+  // console.log('hours: ', (diff / 1000 / 60 / 60)) OR 36e5
+  // console.log('DAY: ', Math.abs(today - date) / 36e5 / 24)
+
+  function getDiff(diff) {
+    if (diff / 36e5 < 1) {
+      // less than an hour ago
+      return (diff = `${Math.floor(diff / 1000 / 60)}m`);
+    } else if (diff / 36e5 < 24) {
+      // Less than a day ago
+      return (diff = `${Math.floor(diff / 36e5)}hr`);
+    } else if (diff / 36e5 / 24 < 365) {
+      // At least a day ago
+      return (diff = `${Math.floor(diff / 36e5 / 24)}d`);
+    } else {
+      // At least a year ago
+      return (diff = `${Math.floor(diff / 36e5 / 24 / 365)}y`);
+    }
   }
-  console.log(diff)
+
   return (
     <div className={classes.commentContainer}>
       <Link to={`/users/${props.comment.user._id}`}>
@@ -30,7 +43,7 @@ const Comment = (props) => {
               {props.comment.user.username}
             </p>
           </Link>
-          <small>{diff}</small>
+          <small>{getDiff(diff)}</small>
         </section>
         <p className={classes.commentDescription}>
           {props.comment.description}
