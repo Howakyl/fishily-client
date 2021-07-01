f1caa8595a476149c9b9a1c9e4000e0a65938eb6
Howakyl <howakyl@gmail.com>
2021-07-01 13:46:27 -0700

    clicking backdrop hides modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import classes from './Modal.module.css';
 const Modal = (props) => {
   return (
     <>
-    <div className={classes.backdrop}></div>
+    <div className={classes.backdrop} onClick={props.onShowModal}></div>
     <div className={`${classes.modal} ${props.className}`}>
       {props.children}
     </div>
