a1965b5136157abc4d75c32a395df510754d31bd
Howakyl <howakyl@gmail.com>
2021-07-01 13:37:01 -0700

    boilerplate for showing modal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import classes from './Modal.module.css';
+
+
+const Modal = (props) => {
+  return (
+    <>
+    <div className={`${classes.modal} ${props.className}`}>
+      {props.children}
+    </div>
+    </>
+  );
+}
+export default Modal;
--- src/pages/Posts/PostDetailComments.js
+++ src/pages/Posts/PostDetailComments.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Comment from "../../components/Comments/Comment";
 import classes from "./PostDetailComments.module.css";
 
 const PostDetailComments = (props) => {
+  const [ showCommentModal, setShowCommentModal ] = useState(false);
+
   const renderComments = () => {
     return props.comments
       .map((comment) => <Comment comment={comment} key={comment._id} />)
@@ -17,6 +19,7 @@ const PostDetailComments = (props) => {
         <div className={classes.noCommentsContainer}>
           <h2 className={classes.commentsHeader}>No comments (yet!)</h2>
           <button className={`btn btn-primary`}>Be the first to comment</button>
+          {showCommentModal && <Modal>this is a modal</Modal>}
         </div>
       )}
     </div>
