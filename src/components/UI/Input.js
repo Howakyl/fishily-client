49f45475f66ecd4676b132633f8ecfc3fa94b4a8
Howakyl <howakyl@gmail.com>
2021-07-02 10:56:06 -0700

    input UI component

---
+++ src/components/UI/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Input.module.css';
+
+
+const Input = (props) => {
+  return (
+    <div className={` form-group ${classes.input}`}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} className={`${props.className} form-control`} />
+    </div>
+  );
+}
+export default Input;
