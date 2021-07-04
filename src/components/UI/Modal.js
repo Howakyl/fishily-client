4a2f880d8517e8dbf153d0ef1d5bd2c89bdb796e
Howakyl <howakyl@gmail.com>
2021-07-03 19:21:11 -0700

    modal now uses react portal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import classes from './Modal.module.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
 
 const Modal = (props) => {
   return (
     <>
-    <div className={classes.backdrop} onClick={props.onShowModal}></div>
-    <div className={`${classes.modal} ${props.className}`}>
-      {props.children}
-    </div>
+      {ReactDOM.createPortal(
+        <>
+          <div className={classes.backdrop} onClick={props.onShowModal}></div>
+          <div className={`${classes.modal} ${props.className}`}>
+            {props.children}
+          </div>
+        </>,
+        document.getElementById("modal-root")
+      )}
     </>
   );
-}
+};
 export default Modal;
--- src/components/UI/Tooltip.js
+++ src/components/UI/Tooltip.js
@@ -4,7 +4,10 @@ import classes from "./Tooltip.module.css";
 const Tooltip = (props) => {
   return (
     <>
-      <div className={classes.tooltipBackdrop} onClick={props.onShowTooltip}></div>
+      <div
+        className={classes.tooltipBackdrop}
+        onClick={props.onShowTooltip}
+      ></div>
       <div className={`${classes.tooltip} ${props.className}`}>
         {props.children}
       </div>
