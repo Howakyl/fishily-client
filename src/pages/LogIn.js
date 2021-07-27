7538a1125a5d9593741867e42339dff7888c4a4c
Howakyl <howakyle96@live.com>
2021-07-27 16:30:09 -0700

    convert login input to Input component

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import Input from '../components/UI/Input';
 import UserModel from "../models/user";
 import { Redirect } from "react-router-dom";
 
 const LogIn = (props) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [usernameIsValid, setUserameIsValid] = useState(false);
+  const [passwordIsValid, setpasswordIsValid] = useState(false);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -12,8 +15,13 @@ const LogIn = (props) => {
       username: username,
       password: password,
     };
-
     UserModel.login(formData).then((res) => {
+      if (res.data.error === 'incorrect password.') {
+        console.log('password error')
+      }
+      if (res.data.error === 'no user found.') {
+        console.log('username error')
+      }
       props.setUser(res.data);
       localStorage.setItem("user", JSON.stringify(res.data));
     });
@@ -26,7 +34,7 @@ const LogIn = (props) => {
       <div>
         <form className="container" onSubmit={handleFormSubmit}>
           <h1 className="logInTitle">Log In!</h1>
-          <div className="form-group">
+          {/* <div className="form-group">
             <label htmlFor="usernameInput">username</label>
             <input
               onChange={(e) => setUsername(e.target.value)}
@@ -38,7 +46,18 @@ const LogIn = (props) => {
               pattern=".{4,}"
               title="Must be at least 4 characters long."
             />
-          </div>
+          </div> */}
+          <Input 
+            label="Username"
+            input={{
+              id: "usernameInput",
+              type: "text",
+              placeholder: "Enter your username!",
+              value: username,
+            }}
+            onChange={(e) => setUsername(e.target.value)}
+            onIsValid={usernameIsValid}
+          />
           <br />
           <div className="form-group">
             <label htmlFor="passInput">Password</label>
