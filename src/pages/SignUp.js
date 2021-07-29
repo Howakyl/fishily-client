92e7d3bae8492421587fe1d52060a208d57ff62d
Howakyl <howakyle96@live.com>
2021-07-28 17:35:13 -0700

    convert signup inputs to Input component

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Input from '../components/UI/Input';
 import UserModel from "../models/user";
 import { Redirect } from "react-router-dom";
 
@@ -35,70 +36,67 @@ const SignUp = (props) => {
       <div className="signupContainer">
         <form className="container" onSubmit={handleFormSubmit}>
           <h1>Sign Up!</h1>
-          <div className="form-group">
-            <label htmlFor="usernameInput">username</label>
-            <small className="form-text text-muted">required</small>
-            <input
-              onChange={(e) => setUsername(e.target.value)}
-              type="text"
-              className="form-control"
-              id="usernameInput"
-              value={username}
-              name="username"
-              pattern=".{4,}"
-              title="Must be at least 4 characters long."
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="passInput">Password</label>
-            <small className="form-text text-muted">required</small>
-            <input
-              onChange={(e) => setPassword(e.target.value)}
-              type="password"
-              className="form-control"
-              id="passInput"
-              value={password}
-              name="password"
-              pattern=".{4,}"
-              title="Must be at least 4 characters long."
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="firstNameInput">first name</label>
-            <input
-              onChange={(e) => setFirstName(e.target.value)}
-              type="text"
-              className="form-control"
-              id="firstNameInput"
-              value={firstName}
-              name="firstName"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="lastNameInput">last name</label>
-            <input
-              onChange={(e) => setLastName(e.target.value)}
-              type="text"
-              className="form-control"
-              id="lastNameInput"
-              value={lastName}
-              name="lastName"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="bioInput">Create a bio:</label>
-            <textarea
-              onChange={(e) => setBio(e.target.value)}
-              type="text"
-              className="form-control"
-              id="bioInput"
-              value={bio}
-              name="bio"
-              pattern=".{,200}"
-              title="Must be fewer than 200 characters long."
-            />
-          </div>
-
+          <Input 
+            label="username"
+            input={{
+              id: "usernameInput",
+              type: "text",
+              name: "username",
+              required: true,
+              value: username
+            }}
+            requiredText="Must be at least 4 characters long."
+            onChange={(e) => setUsername(e.target.value)}
+            // onIsValid={true}
+          />
+          <Input 
+            label="password"
+            input={{
+              id: "passInput",
+              type: "password",
+              name: "password",
+              required: true,
+              value: password
+            }}
+            requiredText="Must be at least 4 characters long."
+            onChange={(e) => setPassword(e.target.value)}
+            // onIsValid={true}
+          />
+          <Input 
+            label="first name"
+            input={{
+              id:"firstNameInput",
+              type:"text",
+              name:"firstName",
+              value: firstName
+            }}
+            onChange={(e) => setFirstName(e.target.value)}
+            // onIsValid={true}
+          />
+          <Input 
+            label="last name"
+            input={{
+              id:"lastNameInput",
+              type:"text",
+              name:"lastName",
+              value: lastName
+            }}
+            onChange={(e) => setLastName(e.target.value)}
+            // onIsValid={true}
+          />
+          <Input 
+            label="Create a bio:"
+            input={{
+              id: "bioInput",
+              textarea: "true",
+              name: "bio",
+              required: true,
+              value: bio
+            }}
+            requiredText="Must be fewer than 200 characters long."
+            onChange={(e) => setBio(e.target.value)}
+            // onIsValid={true}
+          />
           <button type="submit" className="btn btn-primary">
             Create Account
           </button>
