e174dd08c60e28121033f5e268f0b7d8395bcf07
Howakyl <howakyl@gmail.com>
2021-08-01 19:10:04 -0700

    add User interface to UserCard component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const UserCard = (props) => {
+interface Props {
+  user: {
+    bio?: string;
+    picture: string;
+    _id: string;
+    username: string;
+  };
+}
+
+const UserCard: React.FC<Props> = ({ user }) => {
   function bioText() {
-    if (props.user.bio) {
-      return props.user.bio;
+    if (user.bio) {
+      return user.bio;
     } else {
       return <p>This user has no bio.</p>;
     }
@@ -12,11 +21,11 @@ const UserCard = (props) => {
 
   return (
     <div className="card" style={{ width: "18rem" }}>
-      <img src={props.user.picture} className="card-img-top" alt="user" />
+      <img src={user.picture} className="card-img-top" alt="user" />
       <div className="card-body">
-        <h5 className="card-title text-truncate">{props.user.username}</h5>
+        <h5 className="card-title text-truncate">{user.username}</h5>
         <div className="card-text text-truncate">{bioText()}</div>
-        <Link to={`users/${props.user._id}`} className="btn btn-primary">
+        <Link to={`users/${user._id}`} className="btn btn-primary">
           View Profile
         </Link>
       </div>
